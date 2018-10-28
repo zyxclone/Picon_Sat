73f32fa3854c875d65a3e0db8f20788341817f5b
spainE2 <44529886+spainE2@users.noreply.github.com>
2018-10-28 19:41:38 +0100

    Add files via upload

---
+++ picon.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+carpetapicon='picon.movistar.transparent.19.2'
+ubicacionpicon='/media/hdd/'
+sizepicon='220,132'
+
+echo 'Creamos el enterno descarga'
+mkdir entornopicon 
+cd /entornopicon/
+echo 'Empezamos la descarga de los picones deseados'
+curl https://codeload.github.com/spainE2/Picon_Enigma2_Sat/tar.gz/master | \
+  tar -xz --strip=2 picon.440x264.TDT.Spain.openbox-master/$carpetapicon
+echo 'Descargamos utilidad resize tamaño picon'
+wget https://raw.githubusercontent.com/spainE2/Picon_Enigma2/master/picon_resize.py
+echo 'le damos permiso de ejecucion'
+chmod 755 picon_resize.py
+sed -i 's/440,264/'$sizepicon'/g' "picon_resize.py"
+echo 'Se procede a redimensionar picon'
+python picon_resize.py
+echo 'Movemos los picones redimensionados al directorio que deseemos'
+cp -r /entornopicon/picon/ $ubicacionpicon
+echo 'Borramos el entorno'
+rm -r /entornopicon/
+echo 'Finalizado'
+
+
