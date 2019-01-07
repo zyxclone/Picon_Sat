eb6acdbec00d0a24ed3c98094a08fe83772429c8
pelajustrana <34580098+pelajustrana@users.noreply.github.com>
2019-01-07 17:57:08 +0100

    Script picon.sh fixed

--- picon.sh
+++ picon.sh
@@ -3,13 +3,13 @@
 carpetapicon='picon.movistar.transparent.19.2'
 ubicacionpicon='/media/hdd/'
 sizepicon='220,132'
-
+cd /
 echo 'Creamos el enterno descarga'
 mkdir entornopicon
 cd /entornopicon/
 echo 'Empezamos la descarga de los picones deseados'
 curl https://codeload.github.com/spainE2/Picon_Enigma2_Sat/tar.gz/master | \
-  tar -xz --strip=2 Picon_Enigma2_Sat-master/$carpetapicon
+  tar -xz --strip-components=2 Picon_Enigma2_Sat-master/$carpetapicon
 echo 'Descargamos utilidad resize tamaño picon'
 wget https://raw.githubusercontent.com/spainE2/Picon_Enigma2/master/picon_resize.py
 echo 'le damos permiso de ejecucion'
